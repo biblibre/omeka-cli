6b787cfd5568205ef234d5e57fa7a375e1a0a7eb
Alexis Ben Miloud--Josselin <alexis.benmiloud@biblibre.com>
2017-07-10 17:30:55 +0200

    Add test for `plugin dl`

--- tests/suite/PluginCommandTest.php
+++ tests/suite/PluginCommandTest.php
@@ -80,13 +80,30 @@ Usage:
 (.+\n)*\z/', $output);
     }
 
+    public function testCanDownloadPlugins()
+    {
+        $command = new PluginCommand();
+
+        ob_start();
+        shell_exec('rm -rf ' . getenv('OMEKA_PATH') . '/plugins/BagIt');
+        $command->run(array('quick' => true), array('dl', 'BagIt'), $this->application);
+        $output = ob_get_clean();
+
+        $this->assertNull(null);
+        $this->assertFileExists(getenv('OMEKA_PATH') . '/plugins/BagIt');
+        $this->assertFileIsReadable(getenv('OMEKA_PATH') . '/plugins/BagIt/plugin.ini');
+        $this->assertFileIsReadable(getenv('OMEKA_PATH') . '/plugins/BagIt/BagItPlugin.php');
+        shell_exec('rm -rf ' . getenv('OMEKA_PATH') . '/plugins/BagIt');
+    }
+
     public function testShowPluginsToUpdate()
     {
         $command = new PluginCommand();
 
         ob_start();
-        $command->run(array(), array('ud'), $this->application);
+        $command->run(array(), array('up'), $this->application);
         $output = ob_get_clean();
+
 
         $this->assertRegExp('/(.+\n)*/', $output);
     }
