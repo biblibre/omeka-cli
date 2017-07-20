98926fd73829ecf54a009118c6091264ce5c1c2d
Alexis Ben Miloud--Josselin <alexis.benmiloud@biblibre.com>
2017-07-20 15:26:42 +0200

    Check version of omeka-cli when using release version

--- src/Command/UpgradeCommand.php
+++ src/Command/UpgradeCommand.php
@@ -33,20 +33,23 @@ class UpgradeCommand extends AbstractCommand
         }
 
         if (file_exists(OMEKACLI_PATH . '/.git')) {
+            $c = new Client();
             $localCommitHash = rtrim(shell_exec('git -C ' . OMEKACLI_PATH . ' rev-parse master'), PHP_EOL);
             try {
-                $c = new Client();
                 $remoteCommitHash = $c->api('repo')->commits()->all('biblibre', 'omeka-cli', array())[0]['sha'];
                 if ($localCommitHash == $remoteCommitHash)
                     echo 'omeka-cli is up-to-date.' . PHP_EOL;
                 else
                     echo 'New version of omeka-cli available.' . PHP_EOL;
-//                    shell_exec('git -C ' . OMEKACLI_PATH . '/' . $plugin->name . ' pull');
             } catch (\RuntimeException $e) {
                 echo $e->getMessage() . PHP_EOL;
             }
         } else {
-            shell_exec('git -C ' . OMEKACLI_PATH . ' ls-remote --tags 2>/dev/null | grep -o \'[0-9]\+\.[0-9]\+\.[0-9]\+\' | sort -rV | sed 1q');
+            $remoteVersion = shell_exec('git -C ' . OMEKACLI_PATH . ' ls-remote --tags https://github.com/biblibre/omeka-cli 2>/dev/null | grep -o \'[0-9]\+\.[0-9]\+\.[0-9]\+\' | sort -rV | sed 1q');
+            if (OMEKACLI_VERSION == $remoteVersion)
+                echo 'omeka-cli is up-to-date.' . PHP_EOL;
+            else
+                echo 'New version of omeka-cli available.' . PHP_EOL;
         }
 
         return 0;
