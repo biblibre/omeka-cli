d8349d595dcce1a99eaeb113e45b18bbc0b382e0
Alexis Ben Miloud--Josselin <alexis.benmiloud@biblibre.com>
2017-08-24 16:35:41 +0200

    Add a non-interactive way to install Omeka

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -7,6 +7,9 @@ use OmekaCli\UIUtils;
 
 class InstallCommand extends AbstractCommand
 {
+    protected $dbOptions    = array();
+    protected $omekaOptions = array();
+
     public function getDescription()
     {
         return 'install Omeka';
@@ -14,22 +17,57 @@ class InstallCommand extends AbstractCommand
 
     public function getUsage()
     {
-        $usage = 'Usage:' . PHP_EOL
-               . '    install DIR [VERSION]' . PHP_EOL
-               . PHP_EOL
-               . 'Arguments' . PHP_EOL
-               . '    DIR  the Omeka installation directory' . PHP_EOL
-               . '    VERSION  the Omeka version to install' . PHP_EOL
-               . PHP_EOL
-               . 'Install Omeka. This command needs all the requierements '
-               . 'needed to install Omeka the classic way. See omeka.org '
-               . 'for more informations.' . PHP_EOL;
+        return 'Usage:' . PHP_EOL
+             . '    install [VERSION] DIR [INSTALLATION_ARGS]' . PHP_EOL
+             . PHP_EOL
+             . 'Arguments' . PHP_EOL
+             . '    DIR  the Omeka installation directory' . PHP_EOL
+             . '    VERSION  the Omeka version to install' . PHP_EOL
+             . PHP_EOL
+             . 'Install Omeka. This command needs all the requierements '
+             . 'needed to install Omeka the classic way. See omeka.org '
+             . 'for more informations.' . PHP_EOL
+             . 'When calling this with with the --no-prompt option of '
+             . 'omeka-cli, you will have to pass all needed information '
+             . 'through the argument INSTALLATION_ARGS. Those argument '
+             . 'are in this order:' . PHP_EOL
+             . '- database host' . PHP_EOL
+             . '- database username' . PHP_EOL
+             . '- database user password' . PHP_EOL
+             . '- database name' . PHP_EOL
+             . '- omeka superuser name' . PHP_EOL
+             . '- omeka superuser password' . PHP_EOL
+             . '- omeka superuser email' . PHP_EOL
+             . '- omeka site title' . PHP_EOL
+             . '- omeka admin email' . PHP_EOL
+             . PHP_EOL
+             . 'EXAMPLE:' . PHP_EOL
+             . 'omeka-cli --no-prompt install a_directory localhost '
+             . 'dbuser dbP4ssword dbname root omekaP4ssword root@.yee '
+             . '\'Hello, Omeka!\' admin@example.yee' . PHP_EOL;
 
         return $usage;
     }
 
     public function run($options, $args, Application $application)
     {
+        if (NO_PROMPT) {
+            $this->omekaOptions['admin_email'] = array_pop($args);
+            $this->omekaOptions['site_title']  = array_pop($args);
+            $this->omekaOptions['super_email'] = array_pop($args);
+            $this->omekaOptions['password']    = array_pop($args);
+            $this->omekaOptions['username']    = array_pop($args);
+            $this->dbOptions['dbname']   = array_pop($args);
+            $this->dbOptions['password'] = array_pop($args);
+            $this->dbOptions['username'] = array_pop($args);
+            $this->dbOptions['host']     = array_pop($args);
+            foreach(array_merge($this->dbOptions, $this->omekaOptions) as $option) {
+                if (!$option) {
+                    $this->logger->error($this->getUsage());
+                    return 1;
+                }
+            }
+        }
         if (count($args) == 2) {
             $ver = array_pop($args);
         } else if (count($args) != 1) {
@@ -53,7 +91,10 @@ class InstallCommand extends AbstractCommand
         }
 
         $this->logger->info('configuring database');
-        $this->configDb($dir);
+        if (NO_PROMPT)
+            $this->applyDbConfig($dir);
+        else
+            $this->configDb($dir);
 
         $this->logger->info('checking the database');
         $cwd = getcwd();
@@ -67,7 +108,14 @@ class InstallCommand extends AbstractCommand
         }
 
         $this->logger->info('configuring Omeka');
-        $form = $this->configOmeka();
+        if (NO_PROMPT) {
+            if (!$form = $this->applyOmekaConfig($dir)) {
+                $this->logger->error('something went wrong during Omeka configuration');
+                return 1;
+            }
+        } else {
+            $form = $this->configOmeka();
+        }
 
         $this->logger->info('installing Omeka');
         if ($this->installOmeka($form)) {
@@ -128,6 +176,17 @@ class InstallCommand extends AbstractCommand
         return 0;
     }
 
+    protected function applyDbConfig($dir)
+    {
+        $dbini = $dir . '/db.ini';
+        if (preg_match('/XXXXXXX/', file_get_contents($dbini)))
+            copy($dbini . '.changeme', $dbini);
+        exec('sed -i \'0,/XXXXXXX/s//' . $this->dbOptions['host']     . '/\' ' . $dbini);
+        exec('sed -i \'0,/XXXXXXX/s//' . $this->dbOptions['username'] . '/\' ' . $dbini);
+        exec('sed -i \'0,/XXXXXXX/s//' . $this->dbOptions['password'] . '/\' ' . $dbini);
+        exec('sed -i \'0,/XXXXXXX/s//' . $this->dbOptions['dbname']   . '/\' ' . $dbini);
+    }
+
     protected function configDb($dir)
     {
         $dbini = $dir . '/db.ini';
@@ -165,6 +224,40 @@ class InstallCommand extends AbstractCommand
         return 0;
     }
 
+    protected function applyOmekaConfig()
+    {
+        require_once(FORM_DIR . '/Install.php');
+        $form = new \Omeka_Form_Install();
+        $form->init();
+        $formIsValid = $form->isValid(array(
+        'username'            => $this->omekaOptions['username'],
+        'password'            => $this->omekaOptions['password'],
+        'password_confirm'    => $this->omekaOptions['password'],
+        'super_email'         => $this->omekaOptions['super_email'],
+        'site_title'          => $this->omekaOptions['site_title'],
+        'administrator_email' => $this->omekaOptions['admin_email'],
+        'tag_delimiter'               => ',',
+        'fullsize_constraint'         => '800',
+        'thumbnail_constraint'        => '200',
+        'square_thumbnail_constraint' => '200',
+        'per_page_admin'              => '10',
+        'per_page_public'             => '10',
+        ));
+        if (!$formIsValid) {
+            fprintf(STDERR, 'The following fields are not do not match a '
+                          . 'condition.' . PHP_EOL);
+            $errors = $form->getErrors();
+            $errors = array_filter($errors, function($var) {
+                return !empty($var);
+            });
+            foreach (array_keys($errors) as $field)
+                fprintf(STDERR, $field . ': '
+                              . implode(', ', $errors[$field]) . PHP_EOL);
+        }
+
+        return $formIsValid ? $form : null;
+    }
+
     protected function configOmeka()
     {
         require_once(FORM_DIR . '/Install.php');
@@ -176,14 +269,14 @@ class InstallCommand extends AbstractCommand
             fprintf(STDERR, 'password_confirm: ' . "\x1b[8m"); $password_confirm = trim(fgets(STDIN)); fprintf(STDERR, "\x1b[0m");
             fprintf(STDERR, 'super_email: '); $super_email = trim(fgets(STDIN));
             fprintf(STDERR, 'site_title: '); $site_title = trim(fgets(STDIN));
-            fprintf(STDERR, 'administrator_email: '); $administrator_email = trim(fgets(STDIN));
+            fprintf(STDERR, 'admin_email: '); $admin_email = trim(fgets(STDIN));
             $formIsValid = $form->isValid(array(
                 'username' => $username,
                 'password' => $password,
                 'password_confirm' => $password_confirm,
                 'super_email' => $super_email,
                 'site_title' => $site_title,
-                'administrator_email' => $administrator_email,
+                'admin_email' => $admin_email,
                 'tag_delimiter' => ',',
                 'fullsize_constraint' => '800',
                 'thumbnail_constraint' => '200',
@@ -199,10 +292,10 @@ class InstallCommand extends AbstractCommand
                     fprintf(STDERR, $field . ': ' . implode(', ', $errors[$field]) . PHP_EOL);
             }
             fprintf(STDERR, PHP_EOL);
-            fprintf(STDERR, 'username:            ' . $username . PHP_EOL);
-            fprintf(STDERR, 'super_email:         ' . $super_email . PHP_EOL);
-            fprintf(STDERR, 'site_title:          ' . $site_title . PHP_EOL);
-            fprintf(STDERR, 'administrator_email: ' . $administrator_email . PHP_EOL);
+            fprintf(STDERR, 'username:    ' . $username . PHP_EOL);
+            fprintf(STDERR, 'super_email: ' . $super_email . PHP_EOL);
+            fprintf(STDERR, 'site_title:  ' . $site_title . PHP_EOL);
+            fprintf(STDERR, 'admin_email: ' . $admin_email . PHP_EOL);
         } while (!$formIsValid || !UIUtils::confirmPrompt('Are those informations correct?'));
 
         return $form;
