a1b862f5e1f2a7fd9f4e5fe580ac37d41db20f05
Julian Maurice <julian.maurice@biblibre.com>
2017-09-14 12:22:48 +0200

    Use a different autoload path when installed by composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoload = __DIR__ . '/../../autoload.php')) {
+    require_once $autoload;
+} else {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
 
 define('OMEKACLI_VERSION', '1.0.0-alpha.1');
